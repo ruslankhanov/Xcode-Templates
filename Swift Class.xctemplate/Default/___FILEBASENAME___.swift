e930e87e7c35f7652fe89e49182aa684624a1cd3
Ruslan Khanov <hanov@technokratos.com>
2022-02-04 17:40:40 +0300

    Swift Class file template

---
+++ Swift Class.xctemplate/Default/___FILEBASENAME___.swift
@@ -0,0 +1,18 @@
+//___FILEHEADER___
+
+import Foundation
+
+class ___FILEBASENAME___ {
+
+    // MARK: - Instance properties
+
+    // MARK: - Private properties
+
+    // MARK: - Init
+
+    init() {}
+
+    // MARK: - Instance methods
+
+    // MARK: - Private methods
+}
---
+++ Swift Class.xctemplate/IsSubclass/___FILEBASENAME___.swift
@@ -0,0 +1,18 @@
+//___FILEHEADER___
+
+import Foundation
+
+class ___FILEBASENAME___: ___VARIABLE_superclass:identifier___ {
+
+    // MARK: - Instance properties
+
+    // MARK: - Private properties
+
+    // MARK: - Init
+
+    init() {}
+
+    // MARK: - Instance methods
+
+    // MARK: - Private methods
+}
